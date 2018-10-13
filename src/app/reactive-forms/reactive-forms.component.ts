31097c3835ec161fd8ba2cc545e06f9bd8d12ba3
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-10-13 07:26:39 +0530

    reactive forms

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -17,19 +17,53 @@ export class ReactiveFormsComponent implements OnInit {
   user: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
-  ngOnInit():void{
-this.initForm();
+  ngOnInit(): void {
+    this.initForm();
   }
-  
-  initForm():void{
+
+  initForm(): void {
     this.user = this.formBuilder.group({
-      firstName:new FormControl('first Name', [Validators.required]),
+      firstName: new FormControl('first Name', [Validators.required]),
+      lastName: new FormControl('last Name', [Validators.required]),
+      emailId: new FormControl('last Name', [Validators.required]),
       address: this.initAddressGroup(),
+      phones: this.formBuilder.array([])
+    });
+    this.addPhone(3);
+
+    console.log(this.user);
+  }
+  initAddressGroup(): FormGroup {
+    return this.formBuilder.group({
+      street1: new FormControl('str11', [Validators.required]),
+      street2: new FormControl('str222', [Validators.required]),
+      street3: new FormControl('str33', [Validators.required]),
+      city: new FormControl('city', [Validators.required]),
+      state: new FormControl('state', [Validators.required]),
+      country: new FormControl('country', [Validators.required]),
+      zipCode: new FormControl('12121', [Validators.required])
     });
   }
-  initAddressGroup():FormGroup{
+
+  initPhoneGroup(): FormGroup {
     return this.formBuilder.group({
-      street1: new FormControl ('str11',[Validators.required]),
+      country: ['country', [Validators.required]],
+      type: ['type', [Validators.required]],
+      number: ['1223456', [Validators.required]]
     });
+  }
+
+  addPhone(count: number) {
+    for (let i = 0; i < count; i++) {
+      (<FormArray>this.user.controls['phones']).controls.push(
+        this.initPhoneGroup()
+      );
+    }
+  }
+  removePhone(index: number): void {
+    (<FormArray>this.user.controls['phones']).controls.splice(index, 1);
+  }
+  submit() {
+    console.log(JSON.stringify(this.user.value));
   }
 }
